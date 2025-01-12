160cc5fbfc324b8ded9e816409b4eb55d9e8716f
Svinarenko Vladislav <vladushek12@gmail.com>
2025-01-12 12:45:37 +0300

    init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A dataset contain product and attributes for fine-tuning LLMs.',
+    author='Svinarenko V.A.',
+    license='MIT',
+)
